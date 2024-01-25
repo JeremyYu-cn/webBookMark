cc0d51de13d9694c64d4d9db5050a159cefecc54
IchliebedichZhu <54796446@qq.com>
2024-01-25 12:16:26 +0000

    feat: remove error submit

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
-import { createApp } from 'vue';
-import { VueErrorPlugin } from 'check-code-js';
-import App from './App.vue';
-import router from '@/router/index';
-import store from '@/store/index';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "@/router/index";
+import store from "@/store/index";
 
 console.log(import.meta.env.BASE_URL);
 
 const app = createApp(App);
 
-app.use(VueErrorPlugin as any);
 app.use(router);
 app.use(store);
 
-app.mount('#app');
+app.mount("#app");
